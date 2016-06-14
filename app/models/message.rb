795a3668cc190385a85b3c2cdab5a77fd582e1d2
NavyWorks <navycolor03@gmail.com>
2016-06-14 08:20:16 +0000

    validation correction

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,4 +6,7 @@ class Message < ActiveRecord::Base
 
     # 内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
+    
+    # 数字は0以上の整数のみ許可
+    validates :age , numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 end
